bbd99ee98661d996718fb3bfe4d1f832622ba5e5
zhunavaro <63427932+zhunavaro@users.noreply.github.com>
2023-04-12 20:11:19 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    mbit_小车类.CarCtrlSpeed2(mbit_小车类.CarState.Car_Run, 45, 41)
+    mbit_小车类.CarCtrlSpeed2(mbit_小车类.CarState.Car_Run, 50, 46)
     if (mbit_小车类.Avoid_Sensor(mbit_小车类.enAvoidState.OBSTACLE) || mbit_传感器类.Ultrasonic(DigitalPin.P14, DigitalPin.P15) < 20) {
+        mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Stop)
+        basic.pause(300)
         mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_SpinLeft)
-        basic.pause(200)
+        basic.pause(100)
     }
 })
