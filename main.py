850530cb8a853db950a8cbd5ce90e84c83b123be
zhunavaro <63427932+zhunavaro@users.noreply.github.com>
2023-04-12 19:59:59 +0800

    更新 main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+basic.show_icon(IconNames.HAPPY)
+
 def on_forever():
-    pass
+    mbit_小车类.car_ctrl_speed2(mbit_小车类.CarState.CAR_RUN, 45, 41)
+    if mbit_小车类.Avoid_Sensor(mbit_小车类.enAvoidState.OBSTACLE) or mbit_传感器类.ultrasonic(DigitalPin.P14, DigitalPin.P15) < 15:
+        mbit_小车类.car_ctrl(mbit_小车类.CarState.CAR_SPINLEFT)
+        basic.pause(200)
+        basic.pause(1000)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,8 +1,9 @@
-basic.showIcon(IconNames.Heart)
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-    if (mbit_小车类.Avoid_Sensor(mbit_小车类.enAvoidState.OBSTACLE)) {
-        mbit_小车类.CarCtrlSpeed2(mbit_小车类.CarState.Car_Run, 55, 50)
+    if (mbit_小车类.Avoid_Sensor(mbit_小车类.enAvoidState.OBSTACLE) || mbit_传感器类.Ultrasonic(DigitalPin.P14, DigitalPin.P15) < 20) {
+        mbit_小车类.CarCtrlSpeed2(mbit_小车类.CarState.Car_SpinLeft, 45, 41)
     } else {
-        mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Stop)
+        mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Run)
+        basic.pause(200)
     }
 })
