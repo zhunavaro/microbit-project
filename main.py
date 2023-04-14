8d58213772b2e5e72bc3e64cfbbc746f26d34bf6
zhunavaro <63427932+zhunavaro@users.noreply.github.com>
2023-04-14 22:41:17 +0800

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 basic.show_icon(IconNames.HAPPY)
 
 def on_forever():
-    mbit_小车类.car_ctrl_speed2(mbit_小车类.CarState.CAR_RUN, 45, 41)
-    if mbit_小车类.Avoid_Sensor(mbit_小车类.enAvoidState.OBSTACLE) or mbit_传感器类.ultrasonic(DigitalPin.P14, DigitalPin.P15) < 15:
+    mbit_小车类.car_ctrl_speed2(mbit_小车类.CarState.CAR_RUN, 50, 46)
+    if mbit_小车类.Avoid_Sensor(mbit_小车类.enAvoidState.OBSTACLE) or mbit_传感器类.ultrasonic(DigitalPin.P14, DigitalPin.P15) < 20:
+        mbit_小车类.car_ctrl(mbit_小车类.CarState.CAR_STOP)
+        basic.pause(300)
         mbit_小车类.car_ctrl(mbit_小车类.CarState.CAR_SPINLEFT)
-        basic.pause(200)
-        basic.pause(1000)
+        basic.pause(100)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,10 +1,28 @@
+let 右侧距离 = 0
+let 左侧距离 = 0
 basic.showIcon(IconNames.Happy)
+mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 100)
+basic.pause(500)
 basic.forever(function () {
-    mbit_小车类.CarCtrlSpeed2(mbit_小车类.CarState.Car_Run, 50, 46)
+    mbit_小车类.CarCtrlSpeed2(mbit_小车类.CarState.Car_Run, 50, 42)
+    mbit_小车类.RGB_Car_Big2(mbit_小车类.enColor.Green)
     if (mbit_小车类.Avoid_Sensor(mbit_小车类.enAvoidState.OBSTACLE) || mbit_传感器类.Ultrasonic(DigitalPin.P14, DigitalPin.P15) < 20) {
         mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Stop)
+        mbit_小车类.RGB_Car_Big2(mbit_小车类.enColor.Red)
         basic.pause(300)
-        mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_SpinLeft)
-        basic.pause(100)
+        mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 130)
+        basic.pause(500)
+        左侧距离 = mbit_传感器类.Ultrasonic(DigitalPin.P14, DigitalPin.P15)
+        mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 70)
+        basic.pause(500)
+        右侧距离 = mbit_传感器类.Ultrasonic(DigitalPin.P14, DigitalPin.P15)
+        mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 100)
+        if (左侧距离 > 右侧距离) {
+            mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_SpinLeft)
+            basic.pause(100)
+        } else {
+            mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_SpinRight)
+            basic.pause(100)
+        }
     }
 })
