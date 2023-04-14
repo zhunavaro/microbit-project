5a653fa9935f82959508761e1e1786449563e661
zhunavaro <63427932+zhunavaro@users.noreply.github.com>
2023-04-14 23:13:55 +0800

    增加转弯时候led显示方向

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+右侧距离 = 0
+左侧距离 = 0
 basic.show_icon(IconNames.HAPPY)
+mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 100)
+basic.pause(500)
 
 def on_forever():
-    mbit_小车类.car_ctrl_speed2(mbit_小车类.CarState.CAR_RUN, 50, 46)
+    global 左侧距离, 右侧距离
+    mbit_小车类.car_ctrl_speed2(mbit_小车类.CarState.CAR_RUN, 50, 42)
+    mbit_小车类.RGB_Car_Big2(mbit_小车类.enColor.GREEN)
     if mbit_小车类.Avoid_Sensor(mbit_小车类.enAvoidState.OBSTACLE) or mbit_传感器类.ultrasonic(DigitalPin.P14, DigitalPin.P15) < 20:
         mbit_小车类.car_ctrl(mbit_小车类.CarState.CAR_STOP)
+        mbit_小车类.RGB_Car_Big2(mbit_小车类.enColor.RED)
         basic.pause(300)
-        mbit_小车类.car_ctrl(mbit_小车类.CarState.CAR_SPINLEFT)
-        basic.pause(100)
+        mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 130)
+        basic.pause(500)
+        左侧距离 = mbit_传感器类.ultrasonic(DigitalPin.P14, DigitalPin.P15)
+        mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 70)
+        basic.pause(500)
+        右侧距离 = mbit_传感器类.ultrasonic(DigitalPin.P14, DigitalPin.P15)
+        mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 100)
+        if 左侧距离 > 右侧距离:
+            mbit_小车类.car_ctrl(mbit_小车类.CarState.CAR_SPINLEFT)
+            basic.pause(80)
+        else:
+            mbit_小车类.car_ctrl(mbit_小车类.CarState.CAR_SPINRIGHT)
+            basic.pause(80)
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,26 +1,42 @@
 let 右侧距离 = 0
 let 左侧距离 = 0
 basic.showIcon(IconNames.Happy)
-mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 100)
+mbit_小车类.Servo_Car(mbit_小车类.enServo.S1, 100)
 basic.pause(500)
 basic.forever(function () {
     mbit_小车类.CarCtrlSpeed2(mbit_小车类.CarState.Car_Run, 50, 42)
     mbit_小车类.RGB_Car_Big2(mbit_小车类.enColor.Green)
+    basic.showIcon(IconNames.Happy)
     if (mbit_小车类.Avoid_Sensor(mbit_小车类.enAvoidState.OBSTACLE) || mbit_传感器类.Ultrasonic(DigitalPin.P14, DigitalPin.P15) < 20) {
         mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_Stop)
         mbit_小车类.RGB_Car_Big2(mbit_小车类.enColor.Red)
         basic.pause(300)
-        mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 130)
+        mbit_小车类.Servo_Car(mbit_小车类.enServo.S1, 130)
         basic.pause(500)
         左侧距离 = mbit_传感器类.Ultrasonic(DigitalPin.P14, DigitalPin.P15)
-        mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 70)
+        mbit_小车类.Servo_Car(mbit_小车类.enServo.S1, 70)
         basic.pause(500)
         右侧距离 = mbit_传感器类.Ultrasonic(DigitalPin.P14, DigitalPin.P15)
-        mbit_小车类.Servo_Car(mbit_小车类.enServo.S3, 100)
+        // 舵机复位
+        mbit_小车类.Servo_Car(mbit_小车类.enServo.S1, 100)
         if (左侧距离 > 右侧距离) {
+            basic.showLeds(`
+                . . # . .
+                . . . # .
+                # # # # #
+                . . . # .
+                . . # . .
+                `)
             mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_SpinLeft)
             basic.pause(80)
         } else {
+            basic.showLeds(`
+                . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+                `)
             mbit_小车类.CarCtrl(mbit_小车类.CarState.Car_SpinRight)
             basic.pause(80)
         }
